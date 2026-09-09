2343c07015a378552c8cfd3687c067694b19f98d
Shaninja11 <shaninja1100@gmail.com>
2026-09-09 14:55:01 +0100

    - Added functional search provider dropdown. - Cleaned up html formatting.

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-var inputField = document.getElementById("inputField");
+const inputField = document.getElementById("inputField");
+const providerSelect = document.getElementById("chooseprov");
 
 function search() {
-  window.open("https://www.ecosia.org/search?q=" + inputField.value, "_blank");
-};
+  event.preventDefault();
+  let providerUrl;
+
+  switch (providerSelect.value) {
+    case "2":
+      providerUrl = "https://www.startpage.com/do/dsearch?query=";
+      break;
+    case "3":
+      providerUrl = "https://search.brave.com/search?q=";
+      break;
+    case "4":
+      providerUrl = "https://www.qwant.com/?q=";
+      break;
+    default:
+      providerUrl = "https://www.ecosia.org/search?q=";
+  }
+
+  window.open(providerUrl + inputField.value, "_blank");
+}
